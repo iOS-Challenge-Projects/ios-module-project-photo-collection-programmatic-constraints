9dc39a743483e3cc3eba6704e5414b65a86ce35d
fritzgt <tuanis@live.com>
2020-02-12 17:06:15 +0200

    initial commit

--- PhotoCollection/PhotoCollection/Views/PhotoCollectionViewCell.swift
+++ PhotoCollection/PhotoCollection/Views/PhotoCollectionViewCell.swift
@@ -12,5 +12,65 @@ class PhotoCollectionViewCell: UICollectionViewCell {
 
     var photo: Photo?
     
-    
+    func setUpSubviews() {
+        
+        
+        //MARK: - imageView
+        
+        //Creating image view to display photo.imageData
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        //Use to resize image
+        imageView.contentMode = .scaleAspectFit
+        addSubview(imageView)
+        
+        //Image view constraints
+        
+        //top
+        NSLayoutConstraint(item: imageView,
+                           attribute: .top,
+                           relatedBy: .equal,
+                           toItem: self,
+                           attribute: .top,
+                           multiplier: 1,
+            constant: 4).isActive = true
+        
+        //leading
+        NSLayoutConstraint(item: imageView,
+                           attribute: .leading,
+                           relatedBy: .equal,
+                           toItem: self,
+                           attribute: .leading,
+                           multiplier: 1,
+            constant: 4).isActive = true
+        
+        //trailing
+        NSLayoutConstraint(item: imageView,
+                           attribute: .trailing,
+                           relatedBy: .equal,
+                           toItem: self,
+                           attribute: .trailing,
+                           multiplier: 1,
+            constant: -4).isActive = true
+        
+        //height
+        NSLayoutConstraint(item: imageView,
+                              attribute: .height,
+                              relatedBy: .equal,
+                              toItem: imageView,
+                              attribute: .width,
+                              multiplier: 1,
+               constant: 0).isActive = true
+           
+        
+        //MARK: - Label
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        addSubview(label)
+        
+        label
+        
+    }
 }
